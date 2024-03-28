afa47d942abc6c50d16a15499289572e7aa816a8
Cristian Morales <CrisMorales@protonmail.com>
2024-03-27 19:22:14 -0500

    commit before testing paginated journal tab.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,33 +1,55 @@
-import { StyleSheet, FlatList, AppState } from 'react-native';
+import { StyleSheet, FlatList, AppState, Pressable } from 'react-native';
 import { View, Text } from '@/components/Themed';
 import RoutineComponent from '@/components/RoutineComponent';
 import { useSQLiteContext } from 'expo-sqlite/next';
 import useJournalData from '@/utils/useJournalData';
 import { indexDataShape } from '@/components/types/dataTypes';
 import { useCallback, useEffect, useRef, useState } from 'react';
-import { useFocusEffect } from 'expo-router';
-import { indexQueryChecks, journalQuery } from '@/utils/indexQueries';
+import { AntDesign } from '@expo/vector-icons';
+const weekdays: string[] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const todayDateObj = new Date();
+
 
 export default function TabOneScreen() {
   const db = useSQLiteContext();
   const journalData: indexDataShape[] | null = useJournalData(db);
 
+  useEffect(() => {
+    const getAllEntryDates = async () => {
+      const results = await db.getAllAsync(`SELECT DISTINCT entry_date FROM habit_entries ORDER BY entry_date DESC;`)
+      console.log(results);
+    }
+
+    getAllEntryDates();
+  }, [])
 
   return (
     <View style={styles.container}>
       {journalData != null ?
-        <FlatList
-          scrollEnabled={true}
-          data={journalData}
-          keyExtractor={(item, index) => {
-            return (item.routine_data ? item.routine_data.title : 'undefined' + index)
-          }}
-          renderItem={({ item }) => {
-            return (
-              < RoutineComponent routine_data={item.routine_data} routine_habits={item.routine_habits} />
-            )
-          }}
-        /> :
+        <>
+          <View style={{ borderBottomWidth: 1, borderBottomColor: 'gray', height: '7%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Pressable>
+              <AntDesign size={25} style={{ marginHorizontal: 5 }} name="left" color='white' />
+            </Pressable>
+            <Text style={{ fontSize: 16 }}>{weekdays[todayDateObj.getDay()]}, {todayDateObj.toLocaleDateString()}</Text>
+            <Pressable>
+              <AntDesign size={25} style={{ marginHorizontal: 5 }} name="right" color='white' />
+            </Pressable>
+          </View>
+          <FlatList
+            scrollEnabled={true}
+            data={journalData}
+            keyExtractor={(item, index) => {
+              return (item.routine_data ? item.routine_data.title : 'undefined' + index)
+            }}
+            renderItem={({ item }) => {
+              return (
+                < RoutineComponent routine_data={item.routine_data} routine_habits={item.routine_habits} />
+              )
+            }}
+          />
+        </>
+        :
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <Text style={{ textAlign: 'center', textAlignVertical: 'center', fontSize: 24 }}>
             No Habits In the Database
